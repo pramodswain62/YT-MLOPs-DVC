e20c3a80d55e7fe4082cb07af430831269c1254a
Pramod <pramodswain62@gmail.com>
2026-04-18 17:15:00 +0530

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+data = {'name': ['alice', 'bob', 'charlei'],
+        'Age': [30,20,25],
+        'city': ['nk', 'dl', 'mb']}
+
+df = pd.DataFrame(data)
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+df.to_csv(file_path, index=False)
+print('csv file saved to ', file_path)
